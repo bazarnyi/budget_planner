f5d1f12b036df361177461648f5231657aca47ba
Denys Bazarnyi <den.bazarnyi@gmail.com>
2018-03-21 14:47:14 +0200

    Controller specs fixed and extended

--- spec/controllers/home_page_spec.rb
+++ spec/controllers/home_page_spec.rb
@@ -1,13 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe HomePageController do
-  describe "GET #index" do
+  describe 'GET #index' do
     let(:do_request) { get :index }
 
-    it "renders the index template" do
-      do_request
-      expect(response).to have_http_status(:ok)
-      expect(response).to render_template(:index)
+    context 'not logged in user' do
+      it 'redirects to WelcomeController' do
+        do_request
+        expect(response).to have_http_status 302
+        expect(response).to redirect_to controller: 'welcome', action: 'index'
+      end
+    end
+
+    context 'logged in user' do
+
+      user = FactoryBot.create(:user)
+      user.confirm
+
+      it 'renders the index template' do
+        sign_in user
+
+        do_request
+        expect(response).to have_http_status(:ok)
+        expect(response).to render_template(:index)
+      end
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,7 @@ require 'capybara/rails'
 require 'spec_helper'
 
 require 'support/factory_bot'
+require 'devise'
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
@@ -46,7 +47,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -67,4 +68,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  #
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
